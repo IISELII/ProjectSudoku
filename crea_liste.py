6c2abf60948d9dbaa3df5cd46dd74ff3fe865c74
alicia kairouani <alicia.kairouani@gmail.com>
2022-12-12 16:08:29 +0100

    jdghgwrt

--- crea_liste.py
+++ crea_liste.py
@@ -1,17 +1,11 @@
 import random
 
 def random_number():
+    'crée une liste de 9 chiffres'
     randomlist = random.sample(range(1, 10), 9)
     return randomlist
 
 def shuffle(liste):
     'pour melanger une liste'
     random.shuffle(liste)
-    return liste
-
-def change_zero(rows):
-    liste= rows.copy()
-    for index, elem in enumerate(liste):
-        if elem == 0:
-            liste[index] = random.randint(1, 9)
     return liste
