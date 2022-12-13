8ce22fee12efc97ba7d32b2418c2c76f80ef3632
selmane kenzari <Kzselmane@gmail.com>
2022-12-13 10:30:30 +0100

    final checker

--- checker_list.py
+++ checker_list.py
@@ -1,6 +1,8 @@
-import numpy as np
-# créer les grilles de sudoku
-grid1 =  [
+# créer des grilles de sudoku à utiliser sur le checker
+
+#  créer la grille non valide
+
+grille_non_valide =  [
         [7, 0, 0, 0, 0, 0, 0, 9, 0],
         [2, 4, 5, 1, 9, 0, 0, 8, 0],
         [3, 8, 6, 0, 0, 0, 6, 0, 0],
@@ -12,6 +14,8 @@ grid1 =  [
         [1, 7, 9, 3, 2, 8, 4, 5, 6]
     ]
 
+# créer la grille valide et remplie
+
 sudoku_1 = [[4, 7, 9, 3, 6, 8, 2, 1, 5],
             [6, 3, 2, 1, 9, 5, 4, 8, 7],
             [1, 8, 5, 2, 7, 4, 6, 3, 9],
@@ -22,51 +26,106 @@ sudoku_1 = [[4, 7, 9, 3, 6, 8, 2, 1, 5],
             [5, 1, 3, 8, 2, 7, 9, 6, 4],
             [7, 2, 6, 9, 4, 1, 3, 5, 8]]
 
-# definir la fonction qui vérifie la ligne
+# la grille pas finie mais valide
+
+grille_valide_non_remplie = [
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]]
+
+
+grid_valide = []
+
+# On définie la fonction row_check qui va vérifier que
+# toutes les lignes du sudoku ne contiennes pas de doublons
 
 def row_check(grille):
 
-    row_nb = 0
-
+    # boucle for qui va passer sur les 9 lignes du dataset
     for row_nb in range(0, 9):
+
+            # boucle for où i prend les valeurs de 1 à 9 à chaque ligne
             for i in range(1, 10):
+
+                # en utilisant count() on vérifie qu'il n'y a pas de doublon
+                # sur la ligne en question
+                # sinon on retourne False
                 if grille[row_nb].count(i) > 1:
                     return False
+
+    # si toutes les lignes sont validé, on retourne True
     return True
 
+
+# définir la fonction col_check qui va vérifier que
+# toutes les lignes du sudoku ne contiennes pas de doublons
 
 def col_check(grille):
 
-    col_nb = 0
-
+    # boucle for où col_nb prend les valeurs 0 à 9
     for col_nb in range(0, 9):
+
+        # col prend en index col_nb et parcours la colonne de col_nb
         col = [index[col_nb] for index in grille]
+
+        # boucle for où i prend les valeurs de 1 à 9
         for i in range(1, 10):
+
+            # en utilisant count() on vérifie qu'il n'y a pas de doublon
+            # sur la colonne en question
+            # sinon on retourne False
             if col.count(i) > 1:
                 return False
 
+    # si toutes les conditions sont validés
     return True
 
-
-def bloc(x,y) :
-    X=x//3
-    Y=y//3
-    return X,Y
-
+# la fonction find_block qui définie les block
+# et les stock dans la variable l1 sous forme de list de list
 def find_block(grille) :
+
+    # on créer une liste l1 vide
     l1=[]
+
+    # X et Y définissent les 9 blocs du sudoku : (0, 0);(0, 1);(0, 2)
+                                                #(1, 0);(1, 1);(1, 2)
+                                                #(2, 0);(2, 1);(2, 2)
     for X in range(0,3) :
         for Y in range (0,3) :
+
+            # on créer la liste l2 vide
             l2=[]
+
+            # x et y définissent pour chaque blocs leurs 9 cellules
             for x in range (0,3) :
                 for y in range (0,3) :
+
+                    # on ajoute à l2 une liste contenant les cellules d'un bloc
                     l2.append(grille[X * 3+x][Y * 3+y])
+
+            # la liste l2 est ajouter à la liste vide l1, qui finira par avoir
+            # les 9 block en liste de liste
             l1.append(l2)
+
+    # On retourne l1
     return l1
+
+# la fonction checker qui vérifie que les 3 conditions sont remplie
+# pour que le sudoku soit valide
 
 def sudoku_check(grille):
     return row_check(grille) and row_check(find_block(grille)) and col_check(grille)
 
+
+
+print(sudoku_check(grille_non_valide))
+# On importe la librairie time pour voir le temps d'execution de notre checker
 
 import time
 start_time = time.time()
