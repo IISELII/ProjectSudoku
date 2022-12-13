c51e905536b4d249bdf387abc1c033d4a6879176
selmane kenzari <Kzselmane@gmail.com>
2022-12-13 11:42:27 +0100

    fonction qui transforme une grille de n'importe quel type en une grille de type liste

---
+++ to_list.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+# définir la fonction qui va transformer les grilles en grille de type liste
+def to_list(grille):
+    grille_liste = list(grille)
+    return grille_liste
